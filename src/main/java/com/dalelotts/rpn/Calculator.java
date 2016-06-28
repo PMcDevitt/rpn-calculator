010257a977407a3f5919ebc2bb4564fab7d3cbab
telkerallstate <naveen.telker@gmail.com>
2016-06-28 17:37:20 +0100

    Test

--- src/main/java/com/dalelotts/rpn/Calculator.java
+++ src/main/java/com/dalelotts/rpn/Calculator.java
@@ -1,11 +1,15 @@
 /* See the file "LICENSE" for the full license governing this code. */
 package com.dalelotts.rpn;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -26,19 +30,31 @@ final class Calculator {
 	}
 
 	public void run() {
-		final String stnadardOperators = "+,-,*,/";
+		final String standardOperators = "+,-,*,/";
 		output.println("Please enter values followed by operation symbols:\n(Press CTRL+Z to end the program):");
-
 		ArrayList<String> inputs = new ArrayList<>();
+		ArrayList<String> inputnew = new ArrayList<>();
+		List<String> strings = Arrays.asList("one", "two", "three");
 		while (scanner.hasNextLine()) {
 			final String tokenString = scanner.next();
+			//////
+			if(!tokenString.matches("\\d+")){
+				inputnew.add(tokenString);
+			}
+//			String[] myArray = { "this", "is", "a", "sentence" };
+//			String result = Arrays.stream(myArray)
+//					.reduce("", (a,b) -> a + b);
+//			System.out.print("Result Stream = " + result);
+			//for(String s : result){ System.out.println(s + " ");}
+
+			//////
 			if(tokenString.matches("\\d+")){
 				inputs.add(tokenString);
 			}else{
 				if(inputs.size() > 1 || tokenString.contains("%")) {
 					try {
 						String numB = inputs.remove(inputs.size() - 1);
-						if(stnadardOperators.contains(tokenString)){
+						if(standardOperators.contains(tokenString)){
 							inputs.add(Calculate(inputs.remove(inputs.size() - 1) + tokenString + numB));
 						}else{
 							if(tokenString.contains("^")){
@@ -57,6 +73,9 @@ final class Calculator {
 				}
 			}
 			for(String s : inputs){ output.print(s + " ");}
+			output.print("\n");
+			for(String s : inputnew){
+				System.out.print(s + " ");}
 			output.print("\n");
 		}
 	}
